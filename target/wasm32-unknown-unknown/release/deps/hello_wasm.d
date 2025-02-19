b7a235714bbdfcc2712ea9637fcbb5abbdcf6bb9
susumOyaji <sumitomo0210@gmail.com>
2025-02-19 11:33:54 +0900

    up

--- target/wasm32-unknown-unknown/release/deps/hello_wasm.d
+++ target/wasm32-unknown-unknown/release/deps/hello_wasm.d
@@ -1,5 +1,5 @@
-C:\Users\Medical\Documents\GitHub\hello-wasm-master\target\wasm32-unknown-unknown\release\deps\hello_wasm.wasm: src\lib.rs
+/Users/sumitomoshigeru/Documents/GitHub/hello-wasm-master/target/wasm32-unknown-unknown/release/deps/hello_wasm.wasm: src/lib.rs
 
-C:\Users\Medical\Documents\GitHub\hello-wasm-master\target\wasm32-unknown-unknown\release\deps\hello_wasm.d: src\lib.rs
+/Users/sumitomoshigeru/Documents/GitHub/hello-wasm-master/target/wasm32-unknown-unknown/release/deps/hello_wasm.d: src/lib.rs
 
-src\lib.rs:
+src/lib.rs:
