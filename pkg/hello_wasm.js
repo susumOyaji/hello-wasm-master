474d0a0c726e353fdbd92ea94a719de3711ed00e
susumOyaji <sumitomo0210@gmail.com>
2025-02-22 10:56:06 +0900

    up

--- pkg/hello_wasm.js
+++ pkg/hello_wasm.js
@@ -441,7 +441,7 @@ function __wbg_get_imports() {
         const ret = false;
         return ret;
     };
-    imports.wbg.__wbindgen_closure_wrapper124 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper125 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 39, __wbg_adapter_22);
         return ret;
     };
--- target/wasm32-unknown-unknown/release/hello_wasm.d
+++ target/wasm32-unknown-unknown/release/hello_wasm.d
@@ -1,1 +1,1 @@
-C:\Users\Medical\Documents\GitHub\hello-wasm-master\target\wasm32-unknown-unknown\release\hello_wasm.wasm: C:\Users\Medical\Documents\GitHub\hello-wasm-master\src\lib.rs
+/Users/sumitomoshigeru/Documents/GitHub/hello-wasm-master/target/wasm32-unknown-unknown/release/hello_wasm.wasm: /Users/sumitomoshigeru/Documents/GitHub/hello-wasm-master/src/lib.rs
